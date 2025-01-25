45bb34a907549e417ebe1147fdfb983d35f63c4c
elizaleung830 <elizaleung830@gmail.com>
2025-01-26 00:18:47 +1030

    update

--- Rover/camera.py
+++ Rover/camera.py
@@ -1,21 +1,22 @@
-import cv2
+
 import numpy as np
 import socket
 import sys
 import pickle
 import struct
 
-cap=cv2.VideoCapture(0)
-clientsocket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM,0)
-clientsocket.connect(('fe80::b79e:dfb1:2fee:350c',8089,0,0))
+#cap=cv2.VideoCapture(0)
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('169.254.89.180',8089))
 
 while True:
-    ret,frame=cap.read()
+    #ret,frame=cap.read()
     # Serialize frame
-    data = pickle.dumps(frame)
+
+    #data = pickle.dumps(frame)
 
     # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
+    #message_size = struct.pack("L", len(data)) ### CHANGED
 
     # Then data
-    clientsocket.sendall(message_size + data)
+    clientsocket.sendall(1)
