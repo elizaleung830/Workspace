0d62211f42fa8bc79de32f315d8aca8e2f5de96d
Eliza Leung <87836029+elizaleung830@users.noreply.github.com>
2025-01-25 12:25:58 +1030

    change to try another solution

--- Rover/camera.py
+++ Rover/camera.py
@@ -1,15 +1,21 @@
-
-import imagiz
 import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
 
-client = imagiz.Client("cc1", server_ip="192.168.137.1")
-vid = cv2.VideoCapture(0)
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+cap=cv2.VideoCapture(0)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('localhost',8089))
 
 while True:
-    r, frame = vid.read()
-    if r:
-        r, image = cv2.imencode('.jpg', frame, encode_param)
-        client.send(image)
-    else:
-        break
+    ret,frame=cap.read()
+    # Serialize frame
+    data = pickle.dumps(frame)
+
+    # Send message length first
+    message_size = struct.pack("L", len(data)) ### CHANGED
+
+    # Then data
+    clientsocket.sendall(message_size + data)
