6a76a5940c7f2df0204f9ad8cbdf8b3869f81685
elizaleung830 <elizaleung830@gmail.com>
2025-01-26 20:41:22 +1030

    update

---
+++ Rover/Streamer_fastapi.py
@@ -0,0 +1,38 @@
+import cv2
+import time
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
+camera = cv2.VideoCapture(0)  # init the camera
+templates = Jinja2Templates(directory="templates")
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = cv2.resize(frame, (640, 480))  # resize the frame
+            frame = frame.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.03)
+
+
+@app.get('/')
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get('/video_feed')
+def video_feed():
+    return StreamingResponse(gen_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='192.168.1.182', port=12379)
